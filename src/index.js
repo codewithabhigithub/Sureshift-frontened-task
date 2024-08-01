12af94918cb097e2298b83470aaef0409223d094
Abhishek Gupta <109487291+codewithabhigithub@users.noreply.github.com>
2024-08-02 00:52:36 +0530

    backened url change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'https://sureshift-backend-task.onrender.com';
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
